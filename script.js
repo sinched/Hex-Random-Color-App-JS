6c875475fadba1087d5aae457a7d321ee752a04f
Your Name <bmsr30@gmail.com>
2023-11-28 20:12:59 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function changeColor() {
+  let hexNumbers = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+
+  let hexCode = "";
+
+  for (let i = 0; i < 6; i++) {
+    let randomIndex = Math.floor(Math.random() * hexNumbers.length);
+
+    hexCode += hexNumbers[randomIndex];
+  }
+
+  document.getElementById("hex-code").innerHTML = hexCode;
+  document.getElementsByTagName("body")[0].style.background = "#" + hexCode;
+}
